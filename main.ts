01058a469d558b1d244f09b20dd9ce8f212f96b1
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-11-23 07:16:43 -0600

    Refactored session manager

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 import { Application } from "jsr:@oak/oak";
 import User from "./models/user.ts";
+import sessionManager from "./middleware/sessionManager.ts";
 import authenticationRouter from "./controllers/authentication.ts";
 import calendarRouter from "./controllers/calendar.ts";
 import eventsRouter from "./controllers/events.ts";
 import pushNotifications from "./controllers/pushNotifications.ts";
 import staticRouter from "./controllers/static.ts";
 import subscriptionRouter from "./controllers/subscriptions.ts";
-import CalendarDatabase from "./models/calendarDatabase.ts";
 
 /* Start workers */
 
@@ -17,23 +17,7 @@ new Worker(import.meta.resolve("./workers/consumers.ts"), { type: "module" });
 
 const app = new Application<{ user: User | null }>();
 
-app.use(async (ctx, next) => {
-  const cookieValue = await ctx.cookies.get("userId");
-  const userId = cookieValue === undefined ? 0 : Number(cookieValue);
-  let user: User | null;
-  try {
-    user = await CalendarDatabase.getUser(userId);
-  } catch {
-    user = null;
-  }
-  ctx.state.user = user;
-
-  await next();
-
-  if (ctx.state.user == null) ctx.cookies.delete("userId");
-  else ctx.cookies.set("userId", ctx.state.user!.id.toString());
-});
-
+app.use(sessionManager);
 app.use(authenticationRouter.routes());
 app.use(calendarRouter.routes());
 app.use(eventsRouter.routes());
---
+++ middleware/sessionManager.ts
@@ -0,0 +1,20 @@
+import { Context, Next } from "jsr:@oak/oak";
+import User from "../models/user.ts";
+import CalendarDatabase from "../models/calendarDatabase.ts";
+
+export default async (ctx: Context, next: Next) => {
+  const cookieValue = await ctx.cookies.get("userId");
+  const userId = cookieValue === undefined ? 0 : Number(cookieValue);
+  let user: User | null;
+  try {
+    user = await CalendarDatabase.getUser(userId);
+  } catch {
+    user = null;
+  }
+  ctx.state.user = user;
+
+  await next();
+
+  if (ctx.state.user == null) ctx.cookies.delete("userId");
+  else ctx.cookies.set("userId", ctx.state.user!.id.toString());
+}
