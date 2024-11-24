40731a5b134b10d637ac7f7ea80176cfcb9be2d9
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-11-24 13:31:44 -0600

    Fix push notifications to use exchange

--- controllers/pushNotifications.ts
+++ controllers/pushNotifications.ts
@@ -1,19 +1,30 @@
 import { Router } from "jsr:@oak/oak";
-
-const pushWorker = new Worker(
-  import.meta.resolve("../workers/consumers/push.ts"),
-  {
-    type: "module",
-  },
-);
+import { connect } from "https://deno.land/x/amqp@v0.24.0/mod.ts";
 
 const router = new Router();
 
 router.get("/notifications/", async (ctx) => {
   const target = await ctx.sendEvents();
-  pushWorker.onmessage = (e) => {
-    target.dispatchMessage(e.data);
-  };
+  const connection = await connect();
+  const channel = await connection.openChannel();
+
+  await channel.declareQueue({ exclusive: true });
+  await channel.declareExchange({
+    exchange: "pushExchange", type: "fanout", durable: false
+  });
+  await channel.bindQueue({ exchange: "pushExchange" });
+
+  let running = true;
+  target.addEventListener("close", () => { running = false; });
+
+  while (running) {
+    await channel.consume({}, (args, _props, data) => {
+      const json = JSON.parse(new TextDecoder().decode(data));
+      target.dispatchMessage(json.eventdescription);
+    });
+  }
+
+  await connection.close();
 });
 
 export default router;
--- workers/producer.ts
+++ workers/producer.ts
@@ -5,13 +5,18 @@ let running: boolean = true;
 // deno-lint-ignore no-unused-vars
 const onmessage = (msg: boolean) => running = msg;
 
-// Setup the connection, channel, and queues
-const queues: string[] = ["discord", "email", "push"];
+// Setup the connection, channel, queues, and exchange
 const connection = await connect();
 const channel = await connection.openChannel();
+
+const queues: string[] = ["discord", "email"];
 for (const queue in queues) {
   await channel.declareQueue({ queue });
 }
+
+await channel.declareExchange({
+  exchange: "pushExchange", type: "fanout", durable: false
+});
 
 while (running) {
   const sql = CalendarDatabase.getUnpublishedMessages(new Date());
@@ -19,13 +24,20 @@ while (running) {
   const promises: Promise<void>[] = [];
   const eventIds: Set<number> = new Set();
   for await (const [row] of cursor) {
-    if (!queues.includes(row.subscriptiontype)) continue;
-    eventIds.add(row.eventid);
+    let basicPublishArgs = {};
+    if (queues.includes(row.subscriptiontype)) {
+      basicPublishArgs = { routingKey: row.subscriptiontype };
+    } else if (row.subscriptiontype == "push") {
+      basicPublishArgs = { exchange: "pushExchange" };
+    }
+
     const promise = channel.publish(
-      { routingKey: row.subscriptiontype },
+      basicPublishArgs,
       { contentType: "application/json" },
-      new TextEncoder().encode(JSON.stringify(row)),
+      new TextEncoder().encode(JSON.stringify(row))
     );
+
+    eventIds.add(row.eventid);
     promises.push(promise);
   }
   promises.push(CalendarDatabase.setEventsAsBroadcast(eventIds));
