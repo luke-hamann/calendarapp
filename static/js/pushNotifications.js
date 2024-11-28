3bdb166436bab9043f0b69cd11a37f61eec7f139
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-11-28 15:44:43 -0600

    Add web push notification button

--- static/js/pushNotifications.js
+++ static/js/pushNotifications.js
@@ -1,10 +1,40 @@
-// deno-lint-ignore-file
+const hasPermission = () => (Notification.permission == "granted");
+const isWebPushEnabled = () => (localStorage.getItem("webpush") != null);
 
-window.onload = () => {
-  Notification.requestPermission();
-  const eventSource = new EventSource("/notifications/");
-  eventSource.onmessage = (event) => {
-    console.log(event.data);
+// Start listening for notifications
+const eventSource = new EventSource("/notifications/");
+eventSource.onmessage = (event) => {
+  console.log(event.data);
+
+  if (hasPermission() && isWebPushEnabled()) {
+    console.log("Pushing...");
     new Notification(event.data);
-  };
+  }
 };
+
+const button = document.querySelector("#web-push-toggle");
+
+const disableWebPush = () => {
+  button.innerText = "Push OFF";
+  localStorage.removeItem("webpush");
+};
+
+const enableWebPush = () => {
+  Notification.requestPermission()
+    .then((permission) => {
+      if (permission == "granted") {
+        button.innerText = "Push ON";
+        localStorage.setItem("webpush", "");
+      } else {
+        disableWebPush();
+      }
+    });
+};
+
+if (isWebPushEnabled()) enableWebPush();
+
+button.addEventListener('click', () => {
+  console.log("Toggle...");
+  if (isWebPushEnabled()) disableWebPush();
+  else enableWebPush();
+});
