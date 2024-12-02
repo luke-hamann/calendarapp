f973579e3ef2cacf522c7667c2e578231d18b3da
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-12-01 20:02:31 -0600

    Add sessions

--- middleware/sessionManager.ts
+++ middleware/sessionManager.ts
@@ -1,20 +1,37 @@
 import { Context, Next } from "jsr:@oak/oak";
-import User from "../models/user.ts";
 import CalendarDatabase from "../models/calendarDatabase.ts";
 
 export default async (ctx: Context, next: Next) => {
-  const cookieValue = await ctx.cookies.get("userId");
-  const userId = cookieValue === undefined ? 0 : Number(cookieValue);
-  let user: User | null;
-  try {
-    user = await CalendarDatabase.getUser(userId);
-  } catch {
-    user = null;
+  const kv = await Deno.openKv();
+
+  // Initialize the session
+  let sessionId = await ctx.cookies.get("sessionId");
+  if (sessionId == undefined || (await kv.get([sessionId])).key == null) {
+    sessionId = crypto.randomUUID();
+    const expires = new Date(new Date().valueOf() + (1000 * 60 * 60));
+    ctx.cookies.set("sessionId", sessionId, { expires });
+    await kv.set([sessionId], 0);
   }
-  ctx.state.user = user;
+  
+  // Attempt to authenticate the user
+  let wasAuthenticated = false;
+  try {
+    const userId = (await kv.get([sessionId])).value as number;
+    ctx.state.user = await CalendarDatabase.getUser(userId);
+    wasAuthenticated = true;
+  } catch {
+    ctx.state.user = null;
+  }
 
   await next();
 
-  if (ctx.state.user == null) ctx.cookies.delete("userId");
-  else ctx.cookies.set("userId", ctx.state.user!.id.toString());
+  // Logged out
+  if (wasAuthenticated && ctx.state.user == null) {
+    ctx.cookies.delete("sessionId");
+    await kv.delete([sessionId]);
+  
+  // Logged in
+  } else if (!wasAuthenticated && ctx.state.user != null) {
+    await kv.set([sessionId], ctx.state.user!.id);
+  }
 };
