d3f87c07ac38773fdf283eb6294c291679fbd22d
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-12-03 15:37:40 -0600

    Simplify static file serving

--- controllers/static.ts
+++ controllers/static.ts
@@ -1,17 +1,11 @@
-import { Router, send } from "jsr:@oak/oak";
+import { Context, Next } from "jsr:@oak/oak";
 
-const router = new Router();
-
-router.get("/css/main.css", async (ctx) => {
-  await send(ctx, "./static/css/main.css");
-});
-
-router.get("/js/pushNotifications.js", async (ctx) => {
-  await send(ctx, "./static/js/pushNotifications.js");
-});
-
-router.get("/img/icon.png", async (ctx) => {
-  await send(ctx, "./static/img/icon.png");
-});
-
-export default router;
+export default async (context: Context, next: Next) => {
+  try {
+    await context.send({
+      root: `${Deno.cwd()}/static`,
+    });
+  } catch {
+    await next();
+  }
+};
