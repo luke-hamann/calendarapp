1d35c9bb07eb8ace2e51a97b380f087f809aeb00
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-12-03 14:16:50 -0600

    Review subscriptions controller

--- controllers/subscriptions.ts
+++ controllers/subscriptions.ts
@@ -1,7 +1,7 @@
 import { Router } from "jsr:@oak/oak";
 import nunjucks from "npm:nunjucks";
-import Subscription from "../models/subscription.ts";
-import CalendarDatabase from "../models/calendarDatabase.ts";
+import Subscription from "../models/entities/subscription.ts";
+import SubscriptionDatabase from "../models/databases/subscriptionDatabase.ts";
 
 const router = new Router();
 
@@ -15,47 +15,45 @@ router.post("/subscribe/", async (ctx) => {
   const form = await ctx.request.body.form();
 
   const type = form.get("type") ?? "";
-  if (type != 'email' && type != 'discord') {
-    return;
-  }
+  if (!["email", "discord"].includes(type)) return;
 
   const target = form.get("target") ?? "";
   const emailPattern = new RegExp(/^[^\n]+@[^\n]+$/);
   const discordPattern = new RegExp(
     /^https:\/\/discord\.com\/api\/webhooks\/\d{19}\/[\w\d-]{68}$/,
   );
-  const errors = [];
+
+  let error;
   if (type == "email" && !emailPattern.test(target)) {
-    errors.push("Invalid email address.");
+    error = "Invalid email address.";
   } else if (type == "discord" && !discordPattern.test(target)) {
-    errors.push("Invalid discord webhook.");
+    error = "Invalid Discord webhook.";
   }
 
-  if (errors.length > 0) {
+  if (error) {
     ctx.response.body = nunjucks.render(
       "./views/subscriptions/subscribe.html",
-      {
-        error: "Invalid subscription",
-      },
+      { error, currentUser: ctx.state.user },
     );
   }
 
-  const subscription: Subscription = new Subscription(0, type, target);
-  if (type == 'discord') {
+  const subscription = new Subscription(0, type, target);
+  if (type == "discord") {
     subscription.secretToken = target;
   }
 
-  await CalendarDatabase.addSubscription(subscription);
-  ctx.response.body = nunjucks.render("./views/subscriptions/subscribeSuccess.html", {
-    currentUser: ctx.state.user
-  });
+  await SubscriptionDatabase.addSubscription(subscription);
+  ctx.response.body = nunjucks.render(
+    "./views/subscriptions/subscribeSuccess.html",
+    { currentUser: ctx.state.user },
+  );
 });
 
 router.get("/unsubscribe/", (ctx) => {
   const token = ctx.request.url.searchParams.get("token") ?? "";
   ctx.response.body = nunjucks.render(
     "./views/subscriptions/unsubscribe.html",
-    { token, currentUser : ctx.state.user }
+    { token, currentUser: ctx.state.user },
   );
 });
 
@@ -63,25 +61,23 @@ router.post("/unsubscribe/", async (ctx) => {
   const form = await ctx.request.body.form();
   const token = form.get("token") ?? "";
 
-  let subscription: Subscription;
+  let subscription;
   try {
-    subscription = await CalendarDatabase.getSubscriptionByToken(token);
+    subscription = await SubscriptionDatabase.getSubscriptionByToken(token);
   } catch {
     const errors = ["That subscription could not be found."];
     ctx.response.body = nunjucks.render(
       "./views/subscriptions/unsubscribe.html",
-      { token, errors, currentUser: ctx.state.user }
+      { token, errors, currentUser: ctx.state.user },
     );
     return;
   }
 
-  await CalendarDatabase.deleteSubscription(subscription);
+  await SubscriptionDatabase.deleteSubscription(subscription);
   ctx.response.body = nunjucks.render(
     "./views/subscriptions/unsubscribeSuccess.html",
-    {
-      currentUser: ctx.state.user
-    }
+    { currentUser: ctx.state.user },
   );
-})
+});
 
 export default router;
