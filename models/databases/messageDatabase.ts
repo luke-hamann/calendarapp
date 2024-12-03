fe256703696fc35a4aabf81cf7a63eeb69b3aa6f
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-12-03 15:53:50 -0600

    Review message database

---
+++ models/databases/messageDatabase.ts
@@ -0,0 +1,62 @@
+import sql from "./_database.ts";
+import Message from "../entities/message.ts";
+
+interface MessageRow {
+  eventid: number;
+  eventdescription: string;
+  eventtimestamp: string;
+  eventbroadcast: boolean;
+  subscriptionid: number;
+  subscriptiontype: string;
+  subscriptiontarget: string;
+  subscriptionsecrettoken: string;
+}
+
+export default abstract class MessageDatabase {
+  public static async *getUnpublishedMessages(
+    date: Date,
+  ): AsyncGenerator<Message> {
+    const seconds = Math.round(date.getTime() / 1000);
+    const query = sql`
+      SELECT
+        Events.id eventId,
+        Events.description eventDescription,
+        Events.timestamp eventTimestamp,
+        Events.broadcast eventBroadcast,
+        Subscriptions.id subscriptionId,
+        Subscriptions.type subscriptionType,
+        Subscriptions.target subscriptionTarget,
+        Subscriptions.secretToken subscriptionSecretToken
+      FROM Events, Subscriptions
+      WHERE Events.timestamp <= to_timestamp(${seconds}) AND
+        Events.broadcast = true
+      UNION
+      SELECT
+        Events.id eventId,
+        Events.description eventDescription,
+        Events.timestamp eventTimestamp,
+        Events.broadcast eventBroadcast,
+        0 subscriptionId,
+        'push' subscriptionType,
+        '' subscriptionTarget,
+        '' subscriptionSecretToken
+      FROM Events
+      WHERE Events.timestamp <= to_timestamp(${seconds}) AND
+        Events.broadcast = true
+    `;
+
+    const cursor = query.cursor();
+    for await (const [row] of cursor as AsyncIterable<MessageRow[]>) {
+      yield new Message(
+        row.eventid,
+        row.eventdescription,
+        new Date(row.eventtimestamp + " UTC"),
+        row.eventbroadcast,
+        row.subscriptionid,
+        row.subscriptiontype,
+        row.subscriptiontarget,
+        row.subscriptionsecrettoken,
+      );
+    }
+  }
+}
