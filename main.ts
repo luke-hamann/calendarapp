3eb3133cc87878e8ae34c4f8776f02aea1840318
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-12-04 14:52:59 -0600

    Review main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
 import { Application } from "jsr:@oak/oak";
-import User from "./models/user.ts";
-import sessionManager from "./middleware/sessionManager.ts";
+import User from "./models/entities/user.ts";
 import authenticationRouter from "./controllers/authentication.ts";
 import calendarRouter from "./controllers/calendar.ts";
 import eventsRouter from "./controllers/events.ts";
-import pushNotifications from "./controllers/pushNotifications.ts";
-import staticRouter from "./controllers/static.ts";
+import pushNotificationsRouter from "./controllers/pushNotifications.ts";
 import subscriptionRouter from "./controllers/subscriptions.ts";
+import sessionManager from "./middleware/sessionManager.ts";
+import staticFiles from "./middleware/staticFiles.ts";
 
 /* Start workers */
 
 new Worker(import.meta.resolve("./workers/producer.ts"), { type: "module" });
 new Worker(import.meta.resolve("./workers/consumers.ts"), { type: "module" });
 
-/* Start app */
+/* Build app */
 
 const app = new Application<{ user: User | null }>();
 
@@ -21,8 +21,14 @@ app.use(sessionManager);
 app.use(authenticationRouter.routes());
 app.use(calendarRouter.routes());
 app.use(eventsRouter.routes());
-app.use(pushNotifications.routes());
-app.use(staticRouter.routes());
+app.use(pushNotificationsRouter.routes());
 app.use(subscriptionRouter.routes());
+app.use(staticFiles);
+
+app.addEventListener("listen", () => {
+  console.log("Listening...");
+});
+
+/* Run app */
 
 app.listen({ port: 8080 });
