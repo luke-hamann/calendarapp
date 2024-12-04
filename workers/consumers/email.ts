2c1e370a29d926311968d47eaa627688a1713238
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-12-04 11:37:27 -0600

    Review email consumer

--- workers/consumers/email.ts
+++ workers/consumers/email.ts
@@ -1,23 +1,22 @@
-import Consumer from "../../models/consumer.ts";
-import Message from "../../models/message.ts";
-import Subscription from "../../models/subscription.ts";
-import CalendarDatabase from "../../models/calendarDatabase.ts";
+import Consumer from "./_consumer.ts";
+import Message from "../../models/entities/message.ts";
+import Subscription from "../../models/entities/subscription.ts";
+import SubscriptionDatabase from "../../models/databases/subscriptionDatabase.ts";
 
 const EMAIL_ENABLED = Deno.env.get("EMAIL_ENABLED") == "true";
 
 async function handler(message: Message): Promise<void> {
   const text =
-    `${message.eventDescription}\n\n` +
-    `${message.eventTimestamp}\n\n` +
-    `Unsubscribe: ${Deno.env.get("BASE_URL")}/unsubscribe/?token=` +
-    `${message.subscriptionSecretToken}\n\n`;
-  
+    `${message.eventDescription}\n\n${message.eventTimestamp.toLocaleString()}\n\nUnsubscribe: ${
+      Deno.env.get("BASE_URL")
+    }/unsubscribe/?token=${message.subscriptionSecretToken}\n\n`;
+
   const payload = {
     "From": Deno.env.get("EMAIL_USER"),
-    "To": message.subscriptionUrl,
+    "To": message.subscriptionTarget,
     "Subject": "[Coolander] " + message.eventDescription,
     "MessageStream": "broadcast",
-    "TextBody": text
+    "TextBody": text,
   };
 
   if (!EMAIL_ENABLED) {
@@ -25,19 +24,25 @@ async function handler(message: Message): Promise<void> {
     return;
   }
 
-  const response = await fetch("https://api.postmarkapp.com/email", {
-    method: "POST",
-    headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json",
-      "X-Postmark-Server-Token": Deno.env.get("POSTMARK_SERVER_TOKEN")
-    },
-    body: JSON.stringify(payload)
-  });
+  let response;
+  try {
+    response = await fetch("https://api.postmarkapp.com/email", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "X-Postmark-Server-Token": Deno.env.get("EMAIL_POSTMARK_SERVER_TOKEN"),
+      },
+      body: JSON.stringify(payload),
+    });
+  } catch (e) {
+    console.log(e);
+  }
 
-  if (response.status != 200) {
-    await CalendarDatabase.deleteSubscription(
-      new Subscription(0, message.subscriptionUrl, ''));
+  if (response && response.status != 200) {
+    await SubscriptionDatabase.deleteSubscription(
+      new Subscription(message.subscriptionId, "", "", ""),
+    );
   }
 }
 
