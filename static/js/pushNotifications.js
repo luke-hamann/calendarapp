fc1783277ba64b9fffd8561191f0d8a9c115a0cb
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-12-04 07:27:35 -0600

    Review push notifications client

--- static/js/pushNotifications.js
+++ static/js/pushNotifications.js
@@ -1,5 +1,35 @@
+const PUSH_ON_LABEL = "Push&nbsp;ON";
+const PUSH_OFF_LABEL = "Push&nbsp;OFF";
+
 const hasPermission = () => (Notification.permission == "granted");
 const isWebPushEnabled = () => (localStorage.getItem("webpush") != null);
+
+const button = document.querySelector("#web-push-toggle");
+
+const disableWebPush = () => {
+  button.innerHTML = PUSH_OFF_LABEL;
+  localStorage.removeItem("webpush");
+};
+
+const enableWebPush = () => {
+  Notification.requestPermission()
+    .then((permission) => {
+      if (permission == "granted") {
+        button.innerHTML = PUSH_ON_LABEL;
+        localStorage.setItem("webpush", "");
+      } else {
+        disableWebPush();
+      }
+    });
+};
+
+button.addEventListener("click", () => {
+  console.log("Toggle...");
+  if (isWebPushEnabled()) disableWebPush();
+  else enableWebPush();
+});
+
+if (isWebPushEnabled()) enableWebPush();
 
 // Start listening for notifications
 const eventSource = new EventSource("/notifications/");
@@ -11,30 +41,3 @@ eventSource.onmessage = (event) => {
     new Notification("Coolander", { body: event.data, icon: "/img/icon.png" });
   }
 };
-
-const button = document.querySelector("#web-push-toggle");
-
-const disableWebPush = () => {
-  button.innerText = "Push OFF";
-  localStorage.removeItem("webpush");
-};
-
-const enableWebPush = () => {
-  Notification.requestPermission()
-    .then((permission) => {
-      if (permission == "granted") {
-        button.innerText = "Push ON";
-        localStorage.setItem("webpush", "");
-      } else {
-        disableWebPush();
-      }
-    });
-};
-
-if (isWebPushEnabled()) enableWebPush();
-
-button.addEventListener('click', () => {
-  console.log("Toggle...");
-  if (isWebPushEnabled()) disableWebPush();
-  else enableWebPush();
-});
