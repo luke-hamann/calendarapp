f81c50f09e7ee48353382b083cd461049b8a74fa
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-12-05 06:59:24 -0600

    Add HTML email

--- workers/consumers/email.ts
+++ workers/consumers/email.ts
@@ -5,18 +5,33 @@ import SubscriptionDatabase from "../../models/databases/subscriptionDatabase.ts
 
 const EMAIL_ENABLED = Deno.env.get("EMAIL_ENABLED") == "true";
 
+function escapeHTML(html: string): string {
+  return html.replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;").replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;").replaceAll("'", "&#39;");
+}
+
 async function handler(message: Message): Promise<void> {
-  const text =
-    `${message.eventDescription}\n\n${message.eventTimestamp.toLocaleString()}\n\nUnsubscribe: ${
-      Deno.env.get("BASE_URL")
-    }/unsubscribe/?token=${message.subscriptionSecretToken}\n\n`;
+  const unsubscribeUrl = `${Deno.env.get("BASE_URL")}/unsubscribe/?token=${
+    encodeURIComponent(message.subscriptionSecretToken)
+  }`;
+
+  const text = `${message.eventDescription}\n\n` +
+    `${message.eventTimestamp.toLocaleString()}\n\n` +
+    `Unsubscribe: ${unsubscribeUrl}\n\n`;
+
+  const html = `
+    <p>${escapeHTML(message.eventDescription)}</p>
+    <p>${escapeHTML(message.eventTimestamp.toLocaleString())}</p>
+    <p><a href="${unsubscribeUrl}">Unsubscribe</a></p>`;
 
   const payload = {
     From: Deno.env.get("EMAIL_USER"),
     To: message.subscriptionTarget,
-    Subject: "[Coolander] " + message.eventDescription,
+    Subject: message.eventDescription,
     MessageStream: "broadcast",
     TextBody: text,
+    HtmlBody: html,
   };
 
   if (!EMAIL_ENABLED) {
